2f4c8aacd423378615bc23cbfe9bfbde59ebbe3e
clark chen <chenhuajie1@gmail.com>
2018-04-30 18:52:36 -0400

    add type into freshbooks legacy line

--- lib/freshbooks_legacy/line.rb
+++ lib/freshbooks_legacy/line.rb
@@ -1,7 +1,7 @@
 module FreshBooksLegacy
   class Line < FreshBooksLegacy::Base
     define_schema do |s|
-      s.string :name, :description, :tax1_name, :tax2_name
+      s.string :name, :description, :tax1_name, :tax2_name, :type
       s.float :unit_cost, :tax1_percent, :tax2_percent
       s.float :amount, :read_only => true
       s.float :quantity
